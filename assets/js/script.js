89a4c887b8da953508c75f2742ccc7919cc6b717
edgardopina <edgardopina@yahoo.com>
2021-08-16 23:55:06 -0600

    Created basic HTML framework to identify DOM elementsneeded for this challenge

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const QA_SET = [
+	{
+		question: "What are the three foundational Web technologies?",
+		answer: {
+			a: "Web, CSS, Node",
+			b: "HTML, CSS, Javascript",
+			c: "Node, JSON, CSS",
+			d: "HTML, CSS, Java",
+		},
+		correct: "b",
+	},
+	{
+		question: "What are the three foundational Web technologies?",
+		answer: {
+			a: "Web, CSS, Node",
+			b: "HTML, CSS, Javascript",
+			c: "Node, JSON, CSS",
+			d: "HTML, CSS, Java",
+		},
+		correct: "c",
+	},
+];
