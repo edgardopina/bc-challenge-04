c153bd729a148cb8c4303429caad97c73a53fa5e
edgardopina <edgardopina@yahoo.com>
2021-08-18 16:49:12 -0600

    Added Correct/Wrong leyends and sound effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,24 @@ const QA_SET = [
 	{
 		question: "FIRST QUESTION What are the three foundational Web technologies?",
 		answers: ["Web, CSS, Node", "HTML, CSS, JavaScript", "Node, JSON, CSS", "HTML, CSS, Java"],
-		correct: 1,
+		correctAnswer: "Web, CSS, Node",
 	},
 	{
 		question: "SECOND QUESTION What are the three foundational Web technologies?",
 		answers: ["Web, CSS, Node", "HTML, CSS, JavaScript", "Node, JSON, CSS", "HTML, CSS, Java"],
-		correct: 3,
+		correctAnswer: "HTML, CSS, JavaScript",
 	},
 	{
 		question: "THIRD QUESTION What are the three foundational Web technologies?",
 		answers: ["Web, CSS, Node", "HTML, CSS, JavaScript", "Node, JSON, CSS", "HTML, CSS, Java"],
-		correct: 3,
+		correctAnswer: "HTML, CSS, Java",
 	},
 ];
 
 const EXAM_NUMBER_OF_QUESTIONS = 3;
 var questionCounter = 0;
+var correctSound = "./assets/sfx/correctAnswer.mp3";
+var wrongSound = "./assets/sfx/wrongAnswer.mp3";
 
 var startQuizBtn = document.querySelector("#start-quiz");
 var questionBtn = document.querySelector(".answer-buttons");
@@ -28,34 +30,59 @@ var questionDiv = mainDiv.removeChild(document.querySelector("#quiz-question"));
 var allIsDoneDiv = mainDiv.removeChild(document.querySelector("#all-done"));
 var highScores = mainDiv.removeChild(document.querySelector("#high-scores"));
 
-var presentOneQuestion = function () {
-	console.log("questionCounter: ", questionCounter);
-	questionDiv.querySelector("#question").innerHTML = QA_SET[questionCounter].question;
-	for (var i = 0; i < 4; ++i) {
-		questionDiv.querySelector("#btn-" + i).innerHTML = QA_SET[questionCounter].answers[i];
-	}
-};
-var showQuestionDiv = function () {
+var showQuestionDiv = function ()
+{
 	mainDiv.removeChild(startQuizDiv);
 	mainDiv.appendChild(questionDiv);
 };
 
 var processQuestionDiv = function () {
-	// showQuestionDiv();
-	mainDiv.removeChild(startQuizDiv);
-	mainDiv.appendChild(questionDiv);
-	presentOneQuestion();
+	showQuestionDiv();
+	displayOneQuestion();
 	return false;
 };
 
-var processEach = function () {
+var displayOneQuestion = function () {
+	//
+	console.log("questionCounter: ", questionCounter);
+
+	questionDiv.querySelector("#question").innerHTML = QA_SET[questionCounter].question;
+	for (var i = 0; i < 4; ++i) {
+		questionDiv.querySelector("#btn-" + i).innerHTML = QA_SET[questionCounter].answers[i];
+	}
+};
+
+var processEach = function ()
+{
+	// debugger;
+	checkCorrectness();
+
 	questionCounter++;
 	if (questionCounter < EXAM_NUMBER_OF_QUESTIONS) {
-		presentOneQuestion();
+		displayOneQuestion();
 	} else {
 		return false;
 	}
 };
+
+function checkCorrectness() {
+	// debugger;
+	var choice = event.target.innerHTML; // get the choice
+
+	var result = document.querySelector("#right-wrong");
+	var resultSound = document.querySelector("#result-audio");
+
+	if (choice === QA_SET[questionCounter].correctAnswer) {
+		result.textContent = "Correct!";
+		resultSound.src = correctSound;
+	} else {
+		result.textContent = "Wrong!";
+		resultSound.src = wrongSound;
+	}
+		console.log(result, resultSound);
+	resultSound.play();
+	return;
+}
 
 mainDiv.appendChild(startQuizDiv);
 
