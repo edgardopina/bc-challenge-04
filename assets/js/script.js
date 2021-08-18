0f3c813ac88bb6b673b1e22ddfc8cfae0e8632a0
edgardopina <edgardopina@yahoo.com>
2021-08-18 11:38:32 -0600

    Enable start button and first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const QA_SET = [
 	{
-		question: "What are the three foundational Web technologies?",
+		question: "FIRST QUESTION What are the three foundational Web technologies?",
 		answers: ["Web, CSS, Node", "HTML, CSS, JavaScript", "Node, JSON, CSS", "HTML, CSS, Java"],
 		correct: 1,
 	},
@@ -9,33 +9,46 @@ const QA_SET = [
 		answers: ["Web, CSS, Node", "HTML, CSS, JavaScript", "Node, JSON, CSS", "HTML, CSS, Java"],
 		correct: 3,
 	},
+	{
+		question: "THIRD QUESTION What are the three foundational Web technologies?",
+		answers: ["Web, CSS, Node", "HTML, CSS, JavaScript", "Node, JSON, CSS", "HTML, CSS, Java"],
+		correct: 3,
+	},
 ];
 
+const EXAM_NUMBER_OF_QUESTIONS = 3;
+var questionCounter = 0;
 var mainDiv = document.querySelector("main");
 var startQuizDiv = mainDiv.removeChild(document.querySelector("#start-page"));
 var questionDiv = mainDiv.removeChild(document.querySelector("#quiz-question"));
 var allIsDoneDiv = mainDiv.removeChild(document.querySelector("#all-done"));
 var highScores = mainDiv.removeChild(document.querySelector("#high-scores"));
 
+var presentOneQuestion = function () {
+	questionDiv.querySelector("#question").innerHTML = QA_SET[questionCounter].question;
+	for (var i = 0; i < 4; ++i) {
+		questionDiv.querySelector("#btn-" + i).innerHTML = QA_SET[questionCounter].answers[i];
+	}
+};
 var showQuestionDiv = function () {
 	mainDiv.removeChild(startQuizDiv);
 	mainDiv.appendChild(questionDiv);
-	questionDiv.querySelector("#question").innerHTML = QA_SET[0].question;
-	for (var i = 0; i < 4; ++i) {
-		questionDiv.querySelector("#btn-" + i).innerHTML = QA_SET[0].answers[i];
-	}
 };
 
-var presentQuiz = function () {
-	var olEl = document.getElementById("question-group");
-	console.log(olEl);
-	for (var i = 0; i < 2; ++i) {
-		var liTemp = document.createElement("li");
-		// liTemp.innerHTML=
-		// olEl.
-	}
+var processQuestionDiv = function () {
+	showQuestionDiv();
+	presentOneQuestion();
 };
 
 mainDiv.appendChild(startQuizDiv);
-var startQuiz = document.querySelector("#start-quiz");
-startQuiz.addEventListener("click", showQuestionDiv);
+
+var startQuizBtn = document.querySelector("#start-quiz");
+startQuizBtn.addEventListener("click", processQuestionDiv);
+
+var question = document.querySelector("#answer-buttons");
+question.addEventListener("click", function () {
+	questionCounter++;
+	while (questionCounter < EXAM_NUMBER_OF_QUESTIONS) {
+		presentOneQuestion();
+	}
+});
